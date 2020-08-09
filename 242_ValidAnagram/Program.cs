4774e30900655a0fb1f48ca9beb40f0a8a203703
Arthas <chensibo@nuaa.edu.cn>
2020-08-09 14:23:26 +0800

    242_ValidAnagram 有效的字母异位词 done

---
+++ 242_ValidAnagram/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _242_ValidAnagram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    /// <summary>
+    /// 时间复杂度：O(n)
+    /// 空间复杂度：O(1) 常数级别大小的的哈希表
+    /// </summary>
+    public class Solution
+    {
+        public bool IsAnagram(string s, string t)
+        {
+            // verification
+            if (string.IsNullOrEmpty(s) && string.IsNullOrEmpty(s))
+            {
+                return true;
+            }
+
+            if (string.IsNullOrEmpty(s) || string.IsNullOrEmpty(s) || s.Length != t.Length)
+            {
+                return false;
+            }
+
+            // logic
+            int[] dic = new int[26];
+            for (int i = 0; i < s.Length; i++)
+            {
+                char temChar = s[i];
+                dic[temChar - 'a'] += 1;
+
+                char temCharInT = t[i];
+                dic[temCharInT - 'a'] -= 1;
+            }
+
+            foreach (var item in dic)
+            {
+                if (item != 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
