ab6e70d6a51a7da4bd18e98847188bafdaa1cbc1
Arthas <chensibo@nuaa.edu.cn>
2020-08-10 20:30:27 +0800

    94_BinaryTreeInOrderTraversal 二叉树的中序遍历 done

---
+++ 94_BinaryTreeInOrderTraversal/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _94_BinaryTreeInOrderTraversal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    // Definition for a binary tree node.
+    public class TreeNode
+    {
+        public int val;
+        public TreeNode left;
+        public TreeNode right;
+        public TreeNode(int x) { val = x; }
+    }
+
+    /// <summary>
+    /// 时间复杂度：O(n)
+    /// 空间复杂度：O(n)
+    /// </summary>
+    public class Solution
+    {
+        public IList<int> InorderTraversal(TreeNode root)
+        {
+            if (root == null)
+            {
+                return new List<int>();
+            }
+
+            IList<int> result = new List<int>();
+            Stack<TreeNode> stack = new Stack<TreeNode>();
+            TreeNode currentNode = root;
+
+            while (currentNode != null || stack.Count != 0)
+            {
+                while (currentNode != null)
+                {
+                    stack.Push(currentNode);
+                    currentNode = currentNode.left;
+                }
+
+                currentNode = stack.Pop();
+                result.Add(currentNode.val);
+                currentNode = currentNode.right;
+            }
+
+            return result;
+        }
+    }
+}
