9f56931935f16a91649bd0212650938ab56bad0b
Arthas <chensibo@nuaa.edu.cn>
2020-08-14 10:23:32 +0800

    429_N_aryTreeLevelOrderTraversal N叉树的层序遍历 done

---
+++ 429_N-aryTreeLevelOrderTraversal/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _429_N_aryTreeLevelOrderTraversal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    /*
+   // Definition for a Node.*/
+    public class Node
+    {
+        public int val;
+        public IList<Node> children;
+
+        public Node() { }
+
+        public Node(int _val)
+        {
+            val = _val;
+        }
+
+        public Node(int _val, IList<Node> _children)
+        {
+            val = _val;
+            children = _children;
+        }
+    }
+
+    public class Solution
+    {
+        public IList<IList<int>> LevelOrder(Node root)
+        {
+            if (root == null)
+            {
+                return new List<IList<int>>();
+            }
+
+            IList<IList<int>> result = new List<IList<int>>();
+            IList<int> currentLeveList = new List<int>();
+            Queue<Node> queue = new Queue<Node>();
+            int needCall = 0;
+            int nextNeedCall = 0;
+            Node currentNode = root;
+
+            queue.Enqueue(currentNode);
+            needCall = 1;
+
+            while (queue.Count != 0)
+            {
+                currentNode = queue.Dequeue();
+                currentLeveList.Add(currentNode.val);
+                needCall--;
+
+                if (currentNode.children.Count != 0)
+                {
+                    for (int i = 0; i < currentNode.children.Count; i++)
+                    {
+                        queue.Enqueue(currentNode.children[i]);
+                        nextNeedCall++;
+                    }
+                }
+
+                if (needCall == 0)
+                {
+                    needCall = nextNeedCall;
+                    nextNeedCall = 0;
+                    result.Add(currentLeveList);
+                    currentLeveList = new List<int>();
+                }
+            }
+
+            return result;
+        }
+    }
+}
