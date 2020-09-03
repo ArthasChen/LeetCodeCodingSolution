4d9a2e75735bd4aa75528976bcde395456c1642d
Arthas <chensibo@nuaa.edu.cn>
2020-09-03 22:40:51 +0800

    860_LemonadeChange 柠檬水找零 done

---
+++ 860_LemonadeChange/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _860_LemonadeChange
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    public class Solution
+    {
+        public bool LemonadeChange(int[] bills)
+        {
+            // verification
+            if (bills == null || bills.Length <= 0)
+            {
+                return false;
+            }
+
+            // logic
+            int five = 0;
+            int ten = 0;
+
+            for (int i = 0; i < bills.Length; i++)
+            {
+                if (bills[i] == 5)
+                {
+                    five += 1;
+                }
+                else if (bills[i] == 10)
+                {
+                    if (five > 0)
+                    {
+                        five -= 1;
+                        ten += 1;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+                else if (bills[i] == 20)
+                {
+                    if (five > 0 && ten > 0)
+                    {
+                        five -= 1;
+                        ten -= 1;
+                    }
+                    else if (five >= 3)
+                    {
+                        five -= 3;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        }
+    }
+}
