db62c728440c284903f10821b9bef2e887828b47
Arthas <chensibo@nuaa.edu.cn>
2020-09-06 19:22:37 +0800

    455_AssignCookies 分发饼干 done

---
+++ 455_AssignCookies/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _455_AssignCookies
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    public class Solution
+    {
+        public int FindContentChildren(int[] g, int[] s)
+        {
+            // verification
+            if (g == null || s == null || g.Length == 0 || s.Length == 0)
+            {
+                return 0;
+            }
+
+            // logic
+            Array.Sort(g);
+            Array.Sort(s);
+            int maxNums = 0;
+            int childIndex = 0;
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                for (int j = childIndex; j < g.Length; j++)
+                {
+                    if (s[i] >= g[j])
+                    {
+                        maxNums++;
+                        childIndex = j + 1;
+                        break;
+                    }
+                }
+            }
+
+            return maxNums;
+        }
+    }
+}
