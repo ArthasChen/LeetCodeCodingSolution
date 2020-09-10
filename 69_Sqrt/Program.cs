7243685c08213d25ca6489ab1120e55914a83aef
Arthas <chensibo@nuaa.edu.cn>
2020-09-10 23:31:37 +0800

    69_SqrtX x的平方根 done

---
+++ 69_Sqrt/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _69_Sqrt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    public class Solution
+    {
+        public int MySqrt(int x)
+        {
+            int l = 0, r = x, ans = -1;
+            while (l <= r)
+            {
+                int mid = l + (r - l) / 2;
+                if ((long)mid * mid <= x)
+                {
+                    ans = mid;
+                    l = mid + 1;
+                }
+                else
+                {
+                    r = mid - 1;
+                }
+            }
+            return ans;
+        }
+    }
+}
