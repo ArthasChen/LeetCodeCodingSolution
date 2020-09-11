e97bffae2ebd05163df334a79e233bd745a03568
Arthas <chensibo@nuaa.edu.cn>
2020-09-11 23:20:04 +0800

    367_ValidPerfectSquare 有效的完全平方数 done

---
+++ 367_ValidPerfectSquare/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _367_ValidPerfectSquare
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    public class Solution
+    {
+        public bool IsPerfectSquare(int num)
+        {
+            if (num < 2)
+            {
+                return true;
+            }
+
+            long left = 2, right = num / 2, x, guessSquared;
+            while (left <= right)
+            {
+                x = left + (right - left) / 2;
+                guessSquared = x * x;
+                if (guessSquared == num)
+                {
+                    return true;
+                }
+                if (guessSquared > num)
+                {
+                    right = x - 1;
+                }
+                else
+                {
+                    left = x + 1;
+                }
+            }
+            return false;
+        }
+    }
+}
