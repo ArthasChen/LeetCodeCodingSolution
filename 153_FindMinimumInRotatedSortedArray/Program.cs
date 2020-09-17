883a2bbdc929b5c77728255dd6aa98c0f278c481
Arthas <chensibo@nuaa.edu.cn>
2020-09-17 23:46:17 +0800

    153_FindMinimumInRotatedSortedArray 寻找旋转排序数组中的最小值 done

---
+++ 153_FindMinimumInRotatedSortedArray/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _153_FindMinimumInRotatedSortedArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    public class Solution
+    {
+        public int FindMin(int[] nums)
+        {
+            // verification
+            if (nums == null || nums.Length == 0)
+            {
+                return 0;
+            }
+
+            int leftIndex = 0;
+            int rightIndex = nums.Length - 1;
+            int midIndex = 0;
+
+            while (leftIndex <= rightIndex)
+            {
+                midIndex = (rightIndex - leftIndex) / 2 + leftIndex;
+
+                if (nums[midIndex] > nums[leftIndex] && nums[midIndex] > nums[rightIndex])
+                {
+                    leftIndex = midIndex + 1;
+                }
+                else if (nums[midIndex] > nums[leftIndex] && nums[midIndex] < nums[rightIndex])
+                {
+                    return nums[leftIndex];
+                }
+                else if (nums[midIndex] < nums[leftIndex])
+                {
+                    rightIndex = midIndex;
+                }
+                else
+                {
+                    // m == l
+                    if (nums[leftIndex] > nums[rightIndex])
+                    {
+                        return nums[rightIndex];
+                    }
+                    else
+                    {
+                        return nums[leftIndex];
+                    }
+
+                }
+            }
+
+            return 0;
+        }
+    }
+}
